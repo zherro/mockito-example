5488a5221fa8541805f3f9456f8dcb782a8dd3b1
Cleiton Olivera ( Zherro ) <cleiton.ti@outlook.com>
2020-12-15 18:11:24 -0300

    start

---
+++ src/main/java/br/com/zherro/mockito/mockitoexamples/MockitoExamplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zherro.mockito.mockitoexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zherro/mockito/mockitoexamples/MockitoExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zherro.mockito.mockitoexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
